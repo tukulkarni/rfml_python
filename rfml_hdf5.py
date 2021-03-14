24895bd3d84d0063528a3cbb613e8928970290a3
Tejas Kulkarni <tukulkarni@utexas.edu>
2021-03-13 20:26:59 -0600

    new hdf5 dataset functions and tests, changed some unittests

--- rfml_hdf5.py
+++ rfml_hdf5.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 
 
-### ====== General functions ====== ###
+### ====== RFML specific hdf5 functions and wrappers ====== ###
 def is_rfml_hdf5(filename):
 	"""
 		Checks file file_name against conventions of RFML hdf5 data file:
@@ -27,6 +27,100 @@ def is_rfml_hdf5(filename):
 		fid.close()
 		return status
 
+def hdf5_NGA_config_write(filename, simname, x, y, z, icyl, iper, mask, endian='little-endian'):
+	"""
+		Wrapper function to write configuration file for NGA simulations in RFML hdf5 convention
+		function is structured same as matlab function in RFML matlab repository.
+
+		filename : name (and path) of config file to be written
+		simname	 : a long string containing simulation name keywords for init_flow
+		x, y, z	 : coordinate arrays of cell faces
+		icyl	 : integer flag for the use of cylindrical coordinates
+		iper	 : 3 x 1 array for periodicity in the three directions
+		mask	 : 2D numpy integer array of shape (x.size - 1, y.size - 1)
+		endian	 : 'little-endian' or 'big-endian'
+	"""
+	fid = h5py.File(filename, 'w')
+	try:
+		group_id = fid.create_group('/data')
+		# Attributes
+		#-> simulation name
+		write_type = h5py.h5t.C_S1.copy()
+		write_type.set_size(64)
+		write_type.set_strpad(h5py.h5t.STR_SPACEPAD)
+		__oned_attribute_write(group_id, 'simulation_name', write_type, simname)
+		#-> parameters
+		if(endian=='big-endian'):
+			write_type = h5py.h5t.STD_I32BE
+		else:
+			write_type = h5py.h5t.STD_I32LE
+		nx = x.size - 1
+		ny = y.size - 1
+		nz = z.size - 1
+		parameters = np.array([icyl, iper[0], iper[1], iper[2], nx, ny, nz]).astype(write_type)
+		__oned_attribute_write(group_id, 'parameters', write_type, parameters)
+
+		# Datasets
+		#-> mask : type integer
+		__dataset_write(group_id, 'mask', write_type, mask)
+
+		#-> x, y, z : type float
+		if(endian=='big-endian'):
+			write_type = h5py.h5t.IEEE_F64BE
+		else:
+			write_type = h5py.h5t.IEEE_F64LE
+		__dataset_write(group_id, 'x', write_type, x)
+		__dataset_write(group_id, 'y', write_type, y)
+		__dataset_write(group_id, 'z', write_type, z)
+		__dataset_write(group_id, 'xm', write_type, (x[1:nx] + x[0:nx-1])/2.0)
+		__dataset_write(group_id, 'ym', write_type, (y[1:nx] + y[0:nx-1])/2.0)
+		__dataset_write(group_id, 'zm', write_type, (z[1:nx] + z[0:nx-1])/2.0)
+	finally:
+		fid.close()
+
+def hdf5_NGA_data_write(filename, data, t0, dt, endian='little-endian'):
+	"""
+		Wrapper function to write data file for NGA simulations in RFML hdf5 convention
+		function is structured in the same manner as matlab function in the RFML matlab repository.
+
+		filename : name (and path) of data file to be written
+		data	 : dictonary with key = datafield name, value = data in numpy ndarray
+		t0		 : initial time in time_variables attribute (time_variables[1])
+		dt		 : initial time step in time_variables attribute (time_variables[2])
+	"""
+	fid = h5py.File(filename, 'w')
+	try:
+		group_id = fid.create_group('/data')
+		# Attributes
+		#-> datafield_names
+		write_type = h5py.h5t.C_S1.copy()
+		write_type.set_size(8)
+		write_type.set_strpad(h5py.h5t.STR_SPACEPAD)
+		datafield_names = list(data.keys())
+		__oned_attribute_write(group_id, 'datafield_names', write_type, datafield_names)
+		# -> dimensions
+		if(endian=='big-endian'):
+			write_type = h5py.h5t.STD_I32BE
+		else:
+			write_type = h5py.h5t.STD_I32LE
+		nvars = len(datafield_names)
+		data_dims = data[datafield_names[0]].shape
+		dimensions = np.append(np.array(data_dims),nvars)
+		__oned_attribute_write(group_id, 'dimensions', write_type, dimensions)
+		#-> time_variables
+		if (endian == 'big-endian'):
+			write_type = h5py.h5t.IEEE_F64BE
+		else:
+			write_type = h5py.h5t.IEEE_F64LE
+		tvars = np.array([dt, t0])
+		__oned_attribute_write(group_id, 'time_variables', write_type, tvars)
+
+		# datasets
+		for ivar in range(0,nvars):
+			varname = datafield_names[ivar]
+			__dataset_write(group_id, varname, write_type, data[varname])
+	finally:
+		fid.close()
 
 ### ====== Attribute functions ====== ###
 def hdf5_read_attribute(filename, parentpath, attribute_name):
@@ -37,7 +131,7 @@ def hdf5_read_attribute(filename, parentpath, attribute_name):
 		parentpath 		: path of group or data with which the attribute is associated
 		attribute_name 	: name of the attribute
 
-		return type is list, since attributes are one dimensional
+		return type numpy array, since attributes are one dimensional
 	"""
 	# Returns contents of attribute of a group
 	fid = h5py.File(filename, 'r')
@@ -70,8 +164,8 @@ def hdf5_add_attribute(filename, parentpath, attribute_name, attribute_type, att
 						 must contain all necessary info, such as string type with necessary padding and length
 		attribute_data : numpy array with attribute data to be written
 	"""
+	fid = h5py.File(filename, 'r+')
 	try:
-		fid = h5py.File(filename, 'r+')
 		parent = fid.get(parentpath)
 		attribute_manager = parent.attrs
 
@@ -118,8 +212,123 @@ def hdf5_replace_attribute(filename, parentpath, attribute_name, attribute_data)
 		fid.close()
 		print(err)
 
-
 ### ====== Dataset functions ====== ###
+def hdf5_add_dataset(filename, parentpath, dataset_name, data_type, dataset_data):
+	"""
+		Adds a new dataset to existing hdf5 file
+	"""
+	assert(is_rfml_hdf5(filename))
+	fid = h5py.File(filename, 'r+')
+	try:
+		# Write dataset
+		parent = fid.get(parentpath)
+		__dataset_write(parent, dataset_name, data_type, dataset_data)
+
+		# Change attributes 'dimensions' and 'datafield_names'
+		variable_names = hdf5_read_variables(filename)
+		variable_names = np.append(variable_names, np.array([dataset_name]).astype(type(variable_names)))
+		dimensions = hdf5_read_attribute(filename, parentpath, 'dimensions')
+		dimensions[3] = dimensions[3] + 1
+		fid.close()
+
+		hdf5_replace_attribute(filename, parentpath, 'datafield_names', variable_names)
+		hdf5_replace_attribute(filename, parentpath, 'dimensions', dimensions)
+	except Exception as err:
+		print(err)
+		fid.close()
+
+def hdf5_remove_dataset(filename, parentpath, dataset_name):
+	"""
+		Removes a dataset from hdf5 file.
+		Also modifies the attributes 'dimensions' and 'datafield_names'
+
+		filename: path to the hdf5 file
+		parentpath: path to group which contains the dataset
+		datafield_name: name of the dataset to be remmoved
+	"""
+	fid = h5py.File(filename,'r+')
+	parent = fid.get(parentpath)
+
+	try:
+		if(dataset_name in list(parent)):
+			del parent[dataset_name]
+
+			# change attributes if rfml_hdf5 file
+			if(is_rfml_hdf5(filename)):
+				attribute_manager = parent.attrs
+				# datafield_names
+				dataset_names = attribute_manager['datafield_names']
+				dataset_names_str = [x.decode('UTF-8') for x in dataset_names]
+				idx = dataset_names_str.index(dataset_name)
+				dataset_names = np.delete(dataset_names, idx)
+				# dimensions
+				dimensions = attribute_manager['dimensions']
+				dimensions[3] = dimensions[3] - 1
+
+			fid.close()
+			__hdf5_reclaim_space(filename)
+
+			# replace attributes
+			if(is_rfml_hdf5(filename)):
+				hdf5_replace_attribute(filename, parentpath, 'datafield_names', dataset_names)
+				hdf5_replace_attribute(filename, parentpath, 'dimensions', dimensions)
+
+		else:
+			raise NameError("dataset " + datafield_name + "does not exist")
+	except Exception as err:
+		print(err)
+		fid.close()
+
+def hdf5_read_dataset(filename, parentpath, dataset_name):
+	"""
+		Reads a dataset and returns as numpy ndarray
+
+		filename: name of hdf5 file
+		parentpath: path of group that contains the dataset
+		dataset_name : name of the dataset to be read
+	"""
+	fid = h5py.File(filename, 'r')
+	field = np.array(fid.get(parentpath)[dataset_name])
+	fid.close()
+	return field
+
+def hdf5_overwrite_dataset(filename, parentpath, dataset_name, new_dataset):
+	"""
+		Overwrites an existing dataset in hdf5 file with new data
+		Does not check for shape consistency
+
+		filename : name of hdf5 file
+		parentpath: path of group that contains the dataset
+		dataset_name: name of the dataset to be overwritten
+		new_dataset: new data to be written
+	"""
+	fid = h5py.File(filename, 'r+')
+	try:
+		group_id = fid.get(parentpath)
+		data_id = group_id[dataset_name]
+		data_id[...] = new_dataset
+	finally:
+		fid.close()
+
+def hdf5_get_dataset_type(filename, group_path, dataset_name):
+	"""
+		Returns native hdf5 datatype of a dataset
+
+		filename : name of hdf5 file
+		group_path : path inside the file of the containing group ('/' if no group)
+		dataset_name : name of the dataset
+	"""
+	fid = h5py.File(filename, 'r')
+	datatype_id = None
+	try:
+		group_obj_id = fid.get(group_path).id
+		dataset_obj_id = h5py.h5d.open(group_obj_id, __convert_name(dataset_name))
+		datatype_id = dataset_obj_id.get_type()
+	except Exception as err:
+		print(err)
+	finally:
+		fid.close()
+		return datatype_id
 
 ### === Private functions === ###
 def __oned_attribute_write(parent, attribute_name, write_type, attribute_data):
@@ -134,11 +343,29 @@ def __oned_attribute_write(parent, attribute_name, write_type, attribute_data):
 		attribute_data : attribute data - must be convertible to numpy.ndarray
 	"""
 	parent_id = parent.id
-	dimensions = np.array(attribute_data).shape
+	dimensions = (np.array(attribute_data).size,)	# tuple
 	space_id = h5py.h5s.create_simple(dimensions, dimensions)
-	attribute_id = h5py.h5a.create(parent_id, bytes(attribute_name, encoding='utf-8'), write_type, space_id)
+	attribute_id = h5py.h5a.create(parent_id, __convert_name(attribute_name), write_type, space_id)
 	attribute_id.write(np.array(attribute_data).astype(write_type))
 	attribute_id.close()
+
+def __dataset_write(parent, dataset_name, write_type, write_data):
+	"""
+		Private method to write dataset
+		- Uses low level h5py API
+		- Accessed by public methods 'hdf5_add_field', 'hdf5_NGA_data_write'
+
+		parent: high level group reference where dataset is to be added.
+				Attributes 'datafield_names' and 'dimensions' are not modified inside this function.
+		dataset_name : Name of the dataset
+		write_type	: data type
+		write_data	: data to be written, must be convertible to nparray
+	"""
+	parent_id = parent.id
+	dimensions = np.array(write_data).shape
+	space_id = h5py.h5s.create_simple(dimensions, dimensions)
+	data_id = h5py.h5d.create(parent_id, __convert_name(dataset_name), write_type, space_id)
+	data_id.write(h5py.h5s.ALL, space_id, np.array(write_data).astype(write_type), write_type)
 
 
 def __hdf5_reclaim_space(filename):
@@ -148,3 +375,6 @@ def __hdf5_reclaim_space(filename):
 	# repack to temp file
 	cmd = "h5repack -i " + filename + "-o temp.h5 && mv temp.h5 " + filename
 	os.system(cmd)
+
+def __convert_name(string_to_convert):
+	return bytes(string_to_convert, encoding='utf-8')
--- test_rfml_hdf5.py
+++ test_rfml_hdf5.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import numpy.testing as npt
 from rfml_hdf5 import *
 
 
@@ -10,73 +11,126 @@ class TestHdf5Functions(unittest.TestCase):
 	dataset_dims = (10, 10, 10)
 
 	def setUp(self):
-		# Create temporary files for unit testing
-		file_id = h5py.h5f.create(bytes(self.sample_file, encoding='utf-8'), h5py.h5f.ACC_TRUNC)
-		group_id = h5py.h5g.create(file_id, name=bytes('data', encoding='utf-8'))
-		space_id = h5py.h5s.create_simple(self.dataset_dims, self.dataset_dims)
+		# sample hdf5 file with the RFML structure
+		data = {'C': np.zeros(self.dataset_dims)}
+		hdf5_NGA_data_write(self.sample_file, data, 0, 2E-07)
 
-		# data set
-		data_id = h5py.h5d.create(group_id, bytes('C', encoding='utf-8'), h5py.h5t.IEEE_F64LE, space_id)
-		dataset = np.zeros(self.dataset_dims, np.double)
-		data_id.write(h5py.h5s.ALL, h5py.h5s.ALL, dataset)  # all in memory -> all in file
-		data_id.close()
-
-		# attributes
-		space_id = h5py.h5s.create_simple((4,), (4,))
-		attr_dimensions = h5py.h5a.create(group_id, bytes('dimensions', encoding='utf-8'), h5py.h5t.STD_I32LE, space_id)
-		attr_dimensions.write(np.append(np.asarray(self.dataset_dims), 1))
-		attr_dimensions.close()
-
-		str_type = h5py.h5t.C_S1.copy()
-		str_type.set_size(8)
-		str_type.set_strpad(h5py.h5t.STR_SPACEPAD)
-
-		space_id = h5py.h5s.create_simple((1,))
-		attr_names = h5py.h5a.create(group_id, bytes('datafield_names', encoding='utf-8'), str_type, space_id)
-		attr_names.write(np.array([x.ljust(8, ' ') for x in self.dataset_names]).astype(str_type))
-		attr_names.close()
-
-		group_id.close()
-		file_id.close()
-
-		# create generic hdf5 file without group 'data' or attributes
-		file_id = h5py.h5f.create(bytes(self.generic_file, encoding='utf-8'), h5py.h5f.ACC_TRUNC)
-		space_id = h5py.h5s.create_simple(self.dataset_dims, self.dataset_dims)
-		data_id = h5py.h5d.create(file_id, bytes('C', encoding='utf-8'), h5py.h5t.IEEE_F64LE, space_id)
-
-		dataset = np.zeros(self.dataset_dims, np.double)
-		data_id.write(h5py.h5s.ALL, h5py.h5s.ALL, dataset)  # all in memory -> all in file
-		data_id.close()
+		# Manually create generic hdf5 file without group 'data' or required attributes
+		file_id = h5py.File(self.generic_file, 'w')
+		file_id.create_dataset('C', data=data.get('C'))
 		file_id.close()
 
 	def tearDown(self):
 		cmd = "rm " + self.generic_file + " " + self.sample_file
 		os.system(cmd)
 
-		# File & I/O tests
-
+	# === FILE I/O TESTS === #
 	def test_is_rfml_hdf5(self):
 		self.assertFalse(is_rfml_hdf5(self.generic_file))
 		self.assertTrue(is_rfml_hdf5(self.sample_file))
 		self.assertListEqual(hdf5_read_variables(self.sample_file), list(h5py.File(self.sample_file)['data']))
 
-		# Attribute tests
-
+	# === ATTRIBUTE TESTS === #
 	def test_hdf5_read_attribute(self):
 		self.assertListEqual(list(hdf5_read_attribute(self.sample_file, '/data', 'dimensions')), [10, 10, 10, 1])
 		self.assertListEqual(list(hdf5_read_attribute(self.sample_file, '/data', 'datafield_names').astype('str')),
 							 ['C'])
 
 	def test_hdf5_add_attribute(self):
-		hdf5_add_attribute(self.sample_file, '/data', 'time_variables', h5py.h5t.IEEE_F64LE, [1.234E-04, 2.356E-07])
-		self.assertTrue(np.array_equal(hdf5_read_attribute(self.sample_file, '/data', 'time_variables'),
+		hdf5_add_attribute(self.sample_file, '/data', 'temp_attr', h5py.h5t.IEEE_F64LE, [1.234E-04, 2.356E-07])
+		self.assertTrue(np.array_equal(hdf5_read_attribute(self.sample_file, '/data', 'temp_attr'),
 									   np.array([1.234E-04, 2.356E-07])))
 
 	def test_hdf5_replace_attribute(self):
-		hdf5_add_attribute(self.sample_file, '/data', 'time_variables', h5py.h5t.IEEE_F64LE, [1.234E-04, 2.356E-07])
 		hdf5_replace_attribute(self.sample_file, '/data', 'time_variables', [2.468E-04, 4.712E-07])
 		self.assertTrue(np.array_equal(hdf5_read_attribute(self.sample_file, '/data', 'time_variables'),
 									   np.array([2.468E-04, 4.712E-07])))
+
+	def test_hdf5_add_dataset(self):
+		random_dataset = np.random.random_sample(self.dataset_dims)
+		hdf5_add_dataset(self.sample_file, '/data', 'T', h5py.h5t.IEEE_F64LE, random_dataset)
+
+		# compare written array
+		fid = h5py.File(self.sample_file, 'r')
+		read_dataset = np.array(fid.get('/data')['T']).astype(h5py.h5t.IEEE_F64LE)
+		npt.assert_array_almost_equal(random_dataset, read_dataset, 1e-15)
+
+		# check attributes 'datafield_names' and 'dimensions'
+		self.assertTrue('T' in list(fid['data'].attrs['datafield_names'].astype('str')))
+		self.assertTrue('T' in list(fid['data']))
+		self.assertEqual((fid['data'].attrs['dimensions'])[3], 2)
+		fid.close()
+
+	# === DATASET TESTS === #
+	def test_hdf5_remove_dataset(self):
+		hdf5_remove_dataset(self.sample_file, '/data', 'C')
+		fid = h5py.File(self.sample_file, 'r')
+		self.assertTrue('C' not in list(fid['data'].attrs['datafield_names']))
+		self.assertTrue('C' not in list(fid['data']))
+		self.assertEqual((fid['data'].attrs['dimensions'])[3], 0)
+		fid.close()
+
+	def test_hdf5_overwrite_dataset(self):
+		new_data = np.random.random_sample(self.dataset_dims)
+		new_data = new_data.astype(h5py.h5t.IEEE_F32BE)
+		hdf5_overwrite_dataset(self.sample_file, '/data', 'C', new_data)
+
+		# Check that overwritten data is read exactly, even though memory and file types differ
+		npt.assert_array_almost_equal(hdf5_read_dataset(self.sample_file, '/data', 'C'), new_data, 16)
+
+		# Check that overwriting has not changed the dataset type from F64LE to F32LE
+		new_type = hdf5_get_dataset_type(self.sample_file, 'data', 'C')
+		self.assertEqual(new_type, h5py.h5t.IEEE_F64LE)
+
+	# === HDF5 NGA WRAPPER TESTS === #
+	def test_hdf5_NGA_config_write(self):
+		# write temporary config file
+		x = np.linspace(-1.0, 1.0, 6)
+		y = np.linspace(-2.0, 2.0, 11)
+		z = np.linspace(-4.0, 4.0, 21)
+		mask = np.zeros((5, 10)).astype(h5py.h5t.STD_I32LE)
+		hdf5_NGA_config_write('temp_config.h5', 'isotropic_turbulence', x, y, z, 0, [1, 1, 1], mask)
+
+		# Test attributes
+		npt.assert_array_equal(hdf5_read_attribute('temp_config.h5', '/data', 'simulation_name'),
+							   np.array('isotropic_turbulence').astype('|S64'))
+		npt.assert_array_equal(hdf5_read_attribute('temp_config.h5', '/data', 'parameters'),
+							   np.array([0, 1, 1, 1, x.size - 1, y.size - 1, z.size - 1]))
+
+		# Test datasets
+		npt.assert_array_almost_equal(hdf5_read_dataset('temp_config.h5', '/data', 'x'), x, 15)
+		npt.assert_array_almost_equal(hdf5_read_dataset('temp_config.h5', '/data', 'y'), y, 15)
+		npt.assert_array_almost_equal(hdf5_read_dataset('temp_config.h5', '/data', 'z'), z, 15)
+		npt.assert_array_equal(hdf5_read_dataset('temp_config.h5', '/data', 'mask'), mask)
+
+		os.system('rm temp_config.h5')
+
+	def test_hdf5_NGA_data_write(self):
+		test_data_file = 'test_NGA_data.h5'
+		data = {'U': np.random.random_sample(self.dataset_dims),
+				'V': np.random.random_sample(self.dataset_dims),
+				'W': np.random.random_sample(self.dataset_dims)}
+		hdf5_NGA_data_write(test_data_file, data, 1.0E-04, 2E-07, 'little-endian')
+
+		self.assertTrue(is_rfml_hdf5(test_data_file))
+
+		# Attribute tests
+		dimensions = list(self.dataset_dims)
+		dimensions.append(3)
+		npt.assert_array_almost_equal(hdf5_read_attribute(test_data_file, '/data', 'time_variables'),
+										np.array([2E-07, 1.0E-04]).astype(h5py.h5t.IEEE_F64LE), 16)
+		self.assertListEqual(list(hdf5_read_attribute(test_data_file, '/data', 'dimensions')), dimensions)
+		self.assertListEqual(list(hdf5_read_attribute(test_data_file, '/data', 'datafield_names').astype('str')),
+							 list(data.keys()))
+
+		# Dataset tests
+		fid = h5py.File(test_data_file, 'r')
+		self.assertListEqual(list(fid['data']), ['U', 'V', 'W'])
+		npt.assert_array_almost_equal(hdf5_read_dataset(test_data_file, '/data', 'U'), data.get('U'), 16)
+		npt.assert_array_almost_equal(hdf5_read_dataset(test_data_file, '/data', 'V'), data.get('V'), 16)
+		npt.assert_array_almost_equal(hdf5_read_dataset(test_data_file, '/data', 'W'), data.get('W'), 16)
+
+		fid.close()
 
 
 if __name__ == '__main__':
